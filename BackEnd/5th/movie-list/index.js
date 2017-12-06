dac2d2dbacae39a33b9d8ce0f67bb255b69e9eb8
HASBI\ACER <hasbifadillah@outlook.com>
2017-12-06 14:10:20 +0700

    Update 06-12-2017

--- BackEnd/5th/movie-list/index.js
+++ BackEnd/5th/movie-list/index.js
@@ -1,24 +1,49 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const validator = require("express-validator");
 const movie = require("./movie")
+
 
 const app = express();
 
-const myMovie = new movie.Movie("Movie 1", "", 120, 5, ["Actor 1", "Actor 2"], "Action");
-//console.log(myMovie);
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
+app.use(validator());
 
 app.post("/new", (req, res) => {
-    console.log(req.body.title);
-    res.send("Berhasil !!");
+
+    req.checkBody("title", "Error Message ... ").notEmpty();
+    req.checkBody("synopsis", "Error Message ... ").notEmpty();
+    req.checkBody("duration", "Error Message ... ").notEmpty();
+    req.checkBody("rating", "Error Message ... ").notEmpty();
+    req.checkBody("genre", "Error Message ... ").notEmpty();
+
+    var errors = req.validationErrors();
+    if (errors) {
+        res.status(500).send(errors);
+    }
+    else{
+        
+        var m = new movie.Movie(req.body.title, req.body.synopsis, req.body.duration, req.body.rating,req.body.listOfActor, req.body.genre);
+        movie.addNewData(m);
+        res.send(m);
+    }
+
 });
 
-app.get("/all", (req, res) => {});
-app.get("/details", (req, res) => {});
-app.put("/update", (req, res) => {});
-app.delete("/delete", (req, res) => {});
+app.get("/all", (req, res) => {
+    res.send(movie.fetchData());
+});
+app.get("/details", (req, res) => {
+    res.send(movie.getById(req.query.id));
+});
+app.put("/update", (req, res) => {
+    var m = new movie.Movie(req.body.title, req.body.synopsis, req.body.duration, req.body.rating,req.body.listOfActor, req.body.genre);
+    res.send(movie.updateData(m, req.body.id));
+});
+app.delete("/delete", (req, res) => {
+    res.send(movie.deleteData(req.query.id));
+});
 
 // movie list
 // - add new movie [POST]
--- BackEnd/5th/movie-list/movie.js
+++ BackEnd/5th/movie-list/movie.js
@@ -1,5 +1,8 @@
+const fs = require("fs");
+
 class Movie {
     constructor (title, synopsis, duration, rating, actors, genre){
+        this.id = new Date().valueOf();
         this.title = title;
         this.synopsis = synopsis;
         this.duration = duration;
@@ -9,9 +12,78 @@ class Movie {
     }
 }
 
-// const myMovie = new Movie("Movie 1", "", 120, 5, ["Actor 1", "Actor 2"], "Action");
-// console.log(myMovie);
+var fetchData = () => {
+    try {
+        return JSON.parse(fs.readFileSync("data.json"));
+    } catch (error) {
+        return [];
+    }
+};
+
+var addNewData = (obj) => {
+    let array = fetchData();
+    array.push(obj);
+    fs.writeFileSync("data.json", JSON.stringify(array));
+}
+
+var updateData = (obj, id) => {
+    let array = fetchData();
+    let result = {};
+
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i].id + " " + id);
+        if (array[i].id == id) {
+            result = array[i];
+
+            array[i].title = (obj.title) ? obj.title : array[i].title;
+            array[i].synopsis = (obj.synopsis) ? obj.synopsis : array[i].synopsis;
+            array[i].duration = (obj.duration) ? obj.duration : array[i].duration;
+            array[i].rating = (obj.rating) ? obj.rating : array[i].rating;
+            array[i].listOfActor = (obj.listOfActor) ? obj.listOfActor : array[i].listOfActor;
+            array[i].genre = (obj.genre) ? obj.genre : array[i].genre;
+            
+            break;
+        }
+    }
+
+    fs.writeFileSync("data.json", JSON.stringify(array))
+    return result;
+}
+
+var deleteData = (id) => {
+    let array = fetchData();
+    let result = {};
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].id == id) {
+            result = array[i];
+            array.splice(i,1);
+            break;
+        }
+    }
+    fs.writeFileSync("data.json", JSON.stringify(array))
+    return result;
+}
+
+var getById = (id) => {
+    let array = fetchData();
+    let result = {};
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].id == id) {
+            result = array[i];
+            break;
+        }
+    }
+    
+    return result;
+}
 
 module.exports = {
-    Movie
+    Movie,
+    addNewData,
+    fetchData,
+    getById,
+    updateData, 
+    deleteData
 }
